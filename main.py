c314b34bea4ed0a7098bc40594902fb74e664df0
Beatriz Zinko <zinkobeatriz@gmail.com>
2026-07-13 10:49:04 -0300

    Criado menu principal

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 print("=" * 40)
 print(" SISTEMA DE CONTROLE DE ESTOQUE ")
 print("=" * 40)
+
+print("\nMENU PRINCIPAL")
+print("1 - Cadastrar Produto")
+print("2 - Listar Produtos")
+print("3 - Buscar Produto")
+print("4 - Alterar Produto")
+print("5 - Excluir Produto")
+print("0 - Sair")
+
+opcao = input("\nEscolha uma opção: ")
+
+print(f"\nVocê escolheu a opção {opcao}.")
